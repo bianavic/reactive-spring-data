b415dae3ddb32eea8855f3de2eb31a1751bc8a13
bianavic <bianavic@gmail.com>
2021-09-19 14:07:03 -0300

    created route to handle an http get request with a read operation on the service

---
+++ src/main/kotlin/com/microservices/reactive/CustomerHandler.kt
@@ -0,0 +1,18 @@
+package com.microservices.reactive
+
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.ServerResponse.status
+
+@Component
+class CustomerHandler(val customerService: CustomerService) {
+
+    fun get(serverRequest: ServerRequest) =
+        customerService.getCustomer(serverRequest.pathVariable("id").toInt())
+            .flatMap { ok().body(fromObject(it)) }
+            .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
+
+}
