9ea6fc35a5429df23c6efe9b05bf054e0221bd7f
bianavic <bianavic@gmail.com>
2021-09-19 13:46:11 -0300

    used reactive mongo template to handle operations reactively

--- src/main/kotlin/com/microservices/reactive/CustomerRepository.kt
+++ src/main/kotlin/com/microservices/reactive/CustomerRepository.kt
@@ -1,5 +1,25 @@
 package com.microservices.reactive
 
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import javax.annotation.PostConstruct
 
-interface CustomerRepository : ReactiveCrudRepository<Customer, Int>
+@Repository
+class CustomerRepository(private val template: ReactiveMongoTemplate) {
+
+    companion object {
+        val initialCustomers = listOf(Customer(1, "Chris"),
+            Customer(2, "Donna"),
+            Customer(3, "John", Customer.Telephone("+44", "444444444")))
+
+    }
+    @PostConstruct
+    fun initializeRepository() {
+        initialCustomers.map(Customer::toMono).map(this::create).map(Mono<Customer>::subscribe)
+    }
+
+    fun create(customer: Mono<Customer>) = template.save(customer)
+
+}
